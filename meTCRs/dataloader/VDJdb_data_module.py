42ef945a32bb5c951969045508e131260d88d1d7
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2022-06-01 11:14:35 +0200

    Improve class sampling
    - Exclude classes with less samples than the class_size per batch
    - Add linear class sampling method which samples the classes for each batch proportional to their appearance in the data set

--- meTCRs/dataloader/VDJdb_data_module.py
+++ meTCRs/dataloader/VDJdb_data_module.py
@@ -5,16 +5,16 @@ from torch.nn.functional import one_hot
 import pandas as pd
 import numpy as np
 from pytorch_lightning import LightningDataModule
-from sklearn.model_selection import train_test_split
 
 from meTCRs.dataloader.dataset import TCREpitopeDataset
 
 DATA_SEPARATOR = '\t'
 CDR_SEQUENCE_KEY = 'CDR3'
+EPITOPE_KEY = 'Epitope'
 
 
 class VDJdbDataModule(LightningDataModule):
-    def __init__(self, data_path: str, batch_size: int, encoding: str):
+    def __init__(self, data_path: str, batch_size: int, encoding: str, class_sampling_method: str):
         super().__init__()
         self._data_path = data_path
         self._batch_size = batch_size
@@ -23,39 +23,42 @@ class VDJdbDataModule(LightningDataModule):
         self._train_set = None
         self._val_set = None
         self._dimension = None
+        self._class_sampling_method = class_sampling_method
 
     def setup(self, stage: Optional[str] = None, debug=False, seed=1) -> None:
         skip_rows = (lambda i: i > 0 and np.random.choice([True, False], p=[0.99, 0.01])) if debug else None
 
         raw_data = pd.read_csv(self._data_path, sep=DATA_SEPARATOR, skiprows=skip_rows)
 
-        processed_cdr_sequences = self._process_cdr_sequences(raw_data)
+        raw_train_set, raw_val_set = self._train_val_split(raw_data, train_fraction=0.8, random_state=seed)
 
-        self._dimension = processed_cdr_sequences.shape[1]
+        train_set = self._remove_small_class_data(raw_train_set, min_class_size=2)
+        val_set = self._remove_small_class_data(raw_val_set, min_class_size=2)
 
-        epitopes = list(raw_data['Epitope'])
+        sequence_size = self._get_sequence_size(raw_data)
 
-        tcr_train, tcr_val, epitope_train, epitope_val = train_test_split(processed_cdr_sequences,
-                                                                          epitopes,
-                                                                          random_state=seed,
-                                                                          train_size=0.8)
+        processed_cdr_train_sequences = self._process_cdr_sequences(train_set, sequence_size)
+        processed_cdr_val_sequences = self._process_cdr_sequences(val_set, sequence_size)
 
-        self._train_set = TCREpitopeDataset(tcr_data=tcr_train,
-                                            epitope_data=epitope_train,
+        self._dimension = processed_cdr_train_sequences.shape[1]
+
+        self._train_set = TCREpitopeDataset(tcr_data=processed_cdr_train_sequences,
+                                            epitope_data=train_set[EPITOPE_KEY],
                                             batch_size=self._batch_size,
                                             classes_per_batch=self._classes_per_batch,
-                                            total_batches=len(tcr_train) // self._batch_size)
+                                            total_batches=len(processed_cdr_train_sequences) // self._batch_size,
+                                            class_sampling_method=self._class_sampling_method)
 
-        self._val_set = TCREpitopeDataset(tcr_data=tcr_val,
-                                          epitope_data=epitope_val,
+        self._val_set = TCREpitopeDataset(tcr_data=processed_cdr_val_sequences,
+                                          epitope_data=val_set[EPITOPE_KEY],
                                           batch_size=self._batch_size,
                                           classes_per_batch=self._classes_per_batch,
-                                          total_batches=len(tcr_val) // self._batch_size)
+                                          total_batches=len(processed_cdr_val_sequences) // self._batch_size,
+                                          class_sampling_method=self._class_sampling_method)
 
-    def _process_cdr_sequences(self, raw_data):
-        size = self._get_size(raw_data[CDR_SEQUENCE_KEY])
-        trimmed = self._trim(raw_data[CDR_SEQUENCE_KEY])
-        padded = self._pad(trimmed, size)
+    def _process_cdr_sequences(self, data, sequence_size: int):
+        trimmed = self._trim(data[CDR_SEQUENCE_KEY])
+        padded = self._pad(trimmed, sequence_size)
         tokenized = self._tokenize(padded)
         encoded = self._encode(tokenized)
         return encoded
@@ -98,15 +101,28 @@ class VDJdbDataModule(LightningDataModule):
     def _trim(input_data):
         return input_data.apply(lambda x: x[1:-1])
 
-    @staticmethod
-    def _get_size(input_data):
-        return max(input_data.apply(lambda x: len(x)))
-
     @property
     def dimension(self):
         return self._dimension
 
     @property
     def val_data(self):
-        return self._val_set.tcr_data, self._val_set.epitope_data
+        return self._val_set.tcr_data, self._val_set.epitope_list
 
+    @staticmethod
+    def _get_sequence_size(raw_data):
+        return max(raw_data[CDR_SEQUENCE_KEY].apply(lambda x: len(x)))
+
+    @staticmethod
+    def _train_val_split(data: pd.DataFrame, train_fraction: float, random_state: int):
+        train_set = data.sample(frac=train_fraction, random_state=random_state)
+        complement_mask = ~data.index.isin(train_set)
+        val_set = data[complement_mask]
+
+        return train_set, val_set
+
+    @staticmethod
+    def _remove_small_class_data(raw_data, min_class_size):
+        class_sizes = raw_data.groupby(EPITOPE_KEY)[EPITOPE_KEY].transform(len)
+
+        return raw_data[class_sizes >= min_class_size]
--- meTCRs/dataloader/dataset.py
+++ meTCRs/dataloader/dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from torch.utils.data import IterableDataset
 
 from meTCRs.dataloader.utils.list_inversion_dict import list_inversion_dict
@@ -11,22 +12,39 @@ class TCREpitopeDataset(IterableDataset):
     step, for each class a fixed number of samples is drawn.
     """
 
-    def __init__(self, tcr_data, epitope_data, batch_size: int, classes_per_batch: int, total_batches: int):
+    def __init__(self,
+                 tcr_data,
+                 epitope_data: pd.Series,
+                 batch_size: int,
+                 classes_per_batch: int,
+                 total_batches: int,
+                 class_sampling_method: str = "uniform"):
         """
         :param tcr_data: iterable, contains the tcr sequences
-        :param epitope_data: iterable, contains the epitopes related to each
+        :param epitope_data: pandas.Series, contains the epitopes related to each
         :param batch_size: int, the length of each batch
         :param classes_per_batch: int, number of classes each batch should contain, must be smaller or equal than the
                                   total number of classes available in the labels iterable
         :param total_batches: int, total number of batches to be created
+        :param class_sampling_method: str, method to sample classes. Can be 'uniform' or 'linear'.
         """
         self.tcr_data = tcr_data
-        self.epitope_data = epitope_data
-        self.class_dict = list_inversion_dict(epitope_data)
         self.batch_size = batch_size
         self.classes_per_batch = classes_per_batch
         self.total_batches = total_batches
+
+        self.epitope_list = list(epitope_data)
+        self.class_dict = list_inversion_dict(self.epitope_list)
         self.samples_per_class = batch_size // classes_per_batch
+        self.classes = self.class_dict.keys()
+
+        if class_sampling_method == "uniform":
+            self.class_probabilities = None
+        elif class_sampling_method == "linear":
+            class_sizes = epitope_data.value_counts()
+            self.class_probabilities = [class_sizes[cls] / sum(class_sizes) for cls in self.classes]
+        else:
+            raise NotImplementedError("Class sampling method {} is not implemented".format(class_sampling_method))
 
     def __len__(self):
         return self.total_batches * self.batch_size
@@ -35,10 +53,13 @@ class TCREpitopeDataset(IterableDataset):
         samples = []
 
         for _ in range(self.total_batches):
-            classes = np.random.choice(list(self.class_dict.keys()), size=self.classes_per_batch, replace=False)
+            classes = np.random.choice(list(self.classes),
+                                       size=self.classes_per_batch,
+                                       replace=False,
+                                       p=self.class_probabilities)
             for cls in classes:
-                indices = np.random.choice(self.class_dict[cls], size=self.samples_per_class)
+                indices = np.random.choice(self.class_dict[cls], size=self.samples_per_class, replace=False)
                 for idx in indices:
-                    samples.append((np.array(self.tcr_data[idx]), self.epitope_data[idx]))
+                    samples.append((np.array(self.tcr_data[idx]), self.epitope_list[idx]))
 
         return iter(samples)
