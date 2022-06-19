aad89cc2b9445ff75675193dff63fa399b35d14f
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2022-06-19 21:39:54 +0200

    Debug sampling in TCREpitopeDataset

--- meTCRs/dataloader/dataset.py
+++ meTCRs/dataloader/dataset.py
@@ -58,7 +58,6 @@ class TCREpitopeDataset(IterableDataset):
         for _ in range(self.total_batches):
             classes = np.random.choice(list(self.classes),
                                        size=self.classes_per_batch,
-                                       replace=self.use_replacement,
                                        p=self.class_probabilities)
             for cls in classes:
                 indices = np.random.choice(self.class_dict[cls],
