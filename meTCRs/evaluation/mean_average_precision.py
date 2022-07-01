12193778dac06a6fb3f359eb0c900a4f699e56d3
Lennard Schiefelbein <lennard@familie-schiefelbein.de>
2022-07-01 19:56:28 +0200

    Add MeanAveragePrecision at R

---
+++ meTCRs/evaluation/mean_average_precision.py
@@ -0,0 +1,48 @@
+import torch
+
+
+class MeanAveragePrecision:
+    def __init__(self, dist_type: str, R: int, sequences: torch.Tensor, labels):
+        self._dist_type = dist_type
+        self._r = R
+        self._sequences = sequences
+        self._labels = labels
+
+    def __call__(self, model):
+        embedded_sequences = model(self._sequences)
+
+        if self._dist_type == 'l2':
+            pairwise_distances = torch.cdist(embedded_sequences, embedded_sequences)
+        else:
+            raise NotImplementedError('MAP evaluation not implemented for dist_type {}'.format(self._dist_type))
+
+        knn_tensor = pairwise_distances.topk(k=self._r + 1, dim=1, largest=False, sorted=True).indices
+
+        return torch.mean(torch.sum(self._map_at_r(knn_tensor), dim=1))
+
+    def _map_at_r(self, knn_tensor):
+        match_matrix = torch.tensor([])
+
+        for key, knn in enumerate(knn_tensor):
+            match_vector = self._create_match_vector(key, knn)
+            match_matrix = torch.cat([match_matrix, match_vector], dim=0)
+
+        weight_matrix = torch.triu(torch.stack([1/r * torch.ones(self._r) for r in reversed(range(1, self._r+1))]))
+
+        return torch.matmul(match_matrix, weight_matrix) * match_matrix / self._r
+
+    def _create_match_vector(self, key, knn):
+        match = []
+        for idx in reversed(knn):
+            idx = int(idx)
+            if idx != key:
+                match.append(self._labels[idx] == self._labels[key])
+        match_vector = torch.tensor([match[:self._r + 1]])
+        return match_vector
+
+    def _compare(self, i, j):
+        return self._labels[i] == self._labels[j]
+
+
+
+
