e54b667a485111803fcdff31bb65174d7b438962
Lennard Schiefelbein <lennard.schiefelbein@helmholtz-muenchen.de>
2022-07-24 09:24:38 +0200

    Register positional_encoding in transformer

--- meTCRs/models/embeddings/transformer.py
+++ meTCRs/models/embeddings/transformer.py
@@ -17,12 +17,14 @@ class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len):
         super(PositionalEncoding, self).__init__()
 
-        self.positional_encoding = torch.zeros(max_len, d_model)
+        positional_encoding = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        self.positional_encoding[:, 0::2] = torch.sin(position * div_term)
-        self.positional_encoding[:, 1::2] = torch.cos(position * div_term)
-        self.positional_encoding = self.positional_encoding.unsqueeze(1).transpose(0, 1)
+        positional_encoding[:, 0::2] = torch.sin(position * div_term)
+        positional_encoding[:, 1::2] = torch.cos(position * div_term)
+        positional_encoding = positional_encoding.unsqueeze(1).transpose(0, 1)
+
+        self.register_parameter("positional_encoding", nn.Parameter(positional_encoding, requires_grad=False))
 
     def forward(self, x):
         return x + self.positional_encoding[:, :x.size(1)]
